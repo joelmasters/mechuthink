ec9e4977fc490a3ab1228b54904d4670a0cb2af4
joelmasters <joelmasters@gmail.com>
2020-01-27 06:59:34 -0800

    Updates

---
+++ precache-manifest.05c07063ac7d014a66a12308b63418b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03adb8576d16c36ad30dde03615c70a3",
+    "url": "/mechathink/index.html"
+  },
+  {
+    "revision": "4978b078549a6ab31ef8",
+    "url": "/mechathink/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a84950aa64dd00836024",
+    "url": "/mechathink/static/js/2.caa36513.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/mechathink/static/js/2.caa36513.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4978b078549a6ab31ef8",
+    "url": "/mechathink/static/js/main.73109c1e.chunk.js"
+  },
+  {
+    "revision": "5d766f5bbcbeb0acd6f0",
+    "url": "/mechathink/static/js/runtime-main.0e94f2bd.js"
+  }
+]);
