75531a9059093e3d32af7e09ff4a8af43092496c
joelmasters <joelmasters@gmail.com>
2020-01-27 06:58:24 -0800

    added phrases

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import styled from 'styled-components';
+import { phrases } from './phrases';
 
 function App() {
+
+  const [phrase, setPhrase] = useState(phrases[Math.floor(Math.random()*phrases.length)]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Phrase>
+        {phrase}
+      </Phrase>
+      <NextButton onClick={() => 
+        setPhrase(phrases[Math.floor(Math.random()*phrases.length)])
+      }>
+        Next
+      </NextButton>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Phrase = styled.div`
+  font-size: 20px;
+  text-align: center;
+  color: #000;
+  margin-bottom: 100px;
+`;
+
+const NextButton = styled.button`
+  cursor: pointer;
+  background-color: steelblue;
+  color: #FFF;
+  border-radius: 5px;
+  height: 40px;
+  line-height: 40px;
+  width: 120px;
+  font-size: 20px;
+`;
